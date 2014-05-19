4100689b83a9c57dca983f66df571e63cf979e4a
William Makley <wmakley@tortus.com>
2014-05-19 12:55:36 -0400

    keep validation rules closer together

--- app/models/refinery/slides/slide.rb
+++ app/models/refinery/slides/slide.rb
@@ -3,11 +3,11 @@ module Refinery
     class Slide < Refinery::Core::BaseModel
       self.table_name = 'refinery_slides'
 
+      belongs_to :image, :class_name => '::Refinery::Image'
+
       acts_as_indexed :fields => [:title, :link, :content]
 
       validates :title, :presence => true, :uniqueness => true
-
-      belongs_to :image, :class_name => '::Refinery::Image'
       validates :image, :presence => true
 
       attr_accessible :title, :link, :content, :image_id, :hidden, :position
